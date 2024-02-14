1587ca94136f1f55159d51b965be6bda9ab8d89a
ijonghyeon <academy3746@gmail.com>
2024-02-14 22:40:55 +0900

    WebView configure...1

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webview_pro/webview_flutter.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -8,11 +9,23 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  WebViewController? viewController;
+
+  final String mainUrl = 'https://github.com/academy3746';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(),
+      body: SafeArea(
+        child: WebView(
+          initialUrl: mainUrl,
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (controller) {
+            viewController = controller;
+          },
+        ),
+      ),
     );
   }
 }
