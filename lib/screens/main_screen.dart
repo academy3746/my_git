38aaaa3403e89199997399933cc42cf7dd391c37
ijonghyeon <academy3746@gmail.com>
2024-02-16 21:40:39 +0900

    WebView Practice

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -13,18 +13,66 @@ class _MainScreenState extends State<MainScreen> {
 
   final String mainUrl = 'https://github.com/academy3746';
 
+  bool isLoading = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(
-        child: WebView(
-          initialUrl: mainUrl,
-          javascriptMode: JavascriptMode.unrestricted,
-          onWebViewCreated: (controller) {
-            viewController = controller;
-          },
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        centerTitle: true,
+        title: const Text(
+          'MY GIT',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w700,
+          ),
         ),
+        leading: IconButton(
+          onPressed: () {
+            if (viewController != null) {
+              viewController!.reload();
+            }
+          },
+          icon: const Icon(Icons.refresh),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              if (viewController != null) {
+                viewController!.loadUrl(mainUrl);
+              }
+            },
+            icon: const Icon(Icons.home),
+          ),
+        ],
+      ),
+      body: Stack(
+        children: [
+          WebView(
+            initialUrl: mainUrl,
+            javascriptMode: JavascriptMode.unrestricted,
+            onWebViewCreated: (controller) {
+              viewController = controller;
+            },
+            onPageStarted: (url) async {
+              setState(() {
+                isLoading = true;
+              });
+            },
+            onPageFinished: (url) async {
+              setState(() {
+                isLoading = false;
+              });
+            },
+          ),
+          isLoading
+              ? const Center(
+                  child: CircularProgressIndicator.adaptive(),
+                )
+              : Container(),
+        ],
       ),
     );
   }
